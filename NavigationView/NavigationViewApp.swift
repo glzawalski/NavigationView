22acf43096649dbd81290fb53264696620a1f0bb
glzawalski <gabrielzawalski@gmail.com>
2023-06-20 00:45:10 -0300

    Initial project setup

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Gabriel Zawalski on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
